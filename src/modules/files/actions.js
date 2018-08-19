157e8a13ad1206ef91e3a8505bbf8f5572d95423
Blake Hair <blake.hair@gmail.com>
2018-08-19 17:56:25 -0400

    :sparkles: Adding display of images in taggables directory

--- src/modules/files/actions.js
+++ src/modules/files/actions.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 const { app, dialog } = remote
 
-const getPathToTaggables = () => path.join(app.getPath('userData'), 'taggables')
+export const getPathToTaggables = () => path.join(app.getPath('userData'), 'taggables')
 
 const linkFilesToTaggables = (filenames = [], cb) => {
   const taggablePath = getPathToTaggables()
@@ -36,7 +36,6 @@ export const addFiles = () => {
     filters: [{name: 'Images', extensions: ['jpg', 'png', 'gif']}],
     properties: ['openFile', 'multiSelections']
   }, (filePaths) => {
-    console.log('Selected', { filePaths })
     linkFilesToTaggables(filePaths, (err) => {
       if (err) {
         console.log(err)
--- src/modules/files/index.js
+++ src/modules/files/index.js
@@ -1,8 +1,28 @@
+import fs from 'fs'
+import path from 'path'
 import React from 'react'
 
-import { addFiles } from './actions'
+import { addFiles, getPathToTaggables } from './actions'
 
 class Files extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      files: []
+    }
+  }
+
+  componentWillMount() {
+    fs.readdir(getPathToTaggables(), (err, files) => {
+      if (err) {
+        console.log({ err }, 'An error ocurred fetching taggable files')
+        return
+      }
+
+      this.setState({ files })
+    })
+  }
+
   render() {
     return (
       <div>
@@ -12,6 +32,7 @@ class Files extends React.PureComponent {
         >
           + Add Files
         </button>
+        {this.state.files.map((filename) => <img src={path.join(getPathToTaggables(), filename)} />)}
       </div>
     )
   }
