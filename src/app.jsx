bdbb15ba7803b7a4ecab131aa3b8694c0f1d2736
Blake Hair <blake.hair@gmail.com>
2018-08-19 14:54:10 -0400

    :heavy_plus_sign::building_construction: Adding structure to app and adding react-router

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from 'react'
+import {
+  MemoryRouter as Router,
+  Route
+} from 'react-router-dom'
+
+import Drawer from './modules/drawer'
+import Home from './modules/home'
 
 export default class App extends React.Component {
   render() {
-    return (<div>
-      <h2>Welcome to React!</h2>
-    </div>);
+    return (
+      <Router>
+        <div>
+          <Drawer />
+          <Route exact path="/" component={Home}/>
+        </div>
+      </Router>
+    )
   }
 }
