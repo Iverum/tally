f68701f47ea5bc560a3d1e004e22baaa5321fc07
Blake Hair <blake.hair@gmail.com>
2018-08-19 17:10:56 -0400

    :sparkles: Adding ability to hardlink files into app directory

---
+++ src/modules/files/actions.js
@@ -0,0 +1,47 @@
+import { remote } from 'electron'
+import fs from 'fs'
+import path from 'path'
+
+const { app, dialog } = remote
+
+const getPathToTaggables = () => path.join(app.getPath('userData'), 'taggables')
+
+const linkFilesToTaggables = (filenames = [], cb) => {
+  const taggablePath = getPathToTaggables()
+  let fileCount = filenames.length
+  filenames.forEach((filename) => {
+    const linkName = path.join(taggablePath, path.basename(filename))
+    fs.link(filename, linkName, (err) => {
+      fileCount--
+
+      if (err) {
+        cb(err)
+        return
+      }
+
+      if (fileCount <= 0) {
+        cb(null)
+      }
+    })
+  })
+}
+
+export const addFiles = () => {
+  const picturesPath = app.getPath('pictures')
+  dialog.showOpenDialog(remote.getCurrentWindow(), {
+    title: 'Add File',
+    defaultPath: picturesPath,
+    buttonLabel: 'Add',
+    filters: [{name: 'Images', extensions: ['jpg', 'png', 'gif']}],
+    properties: ['openFile', 'multiSelections']
+  }, (filePaths) => {
+    console.log('Selected', { filePaths })
+    linkFilesToTaggables(filePaths, (err) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log('All files linked')
+      }
+    })
+  })
+}
--- src/modules/files/index.js
+++ src/modules/files/index.js
@@ -1,40 +1,15 @@
-import { remote } from 'electron'
-import fs from 'fs'
 import React from 'react'
 
-const { dialog } = remote
-
+import { addFiles } from './actions'
 
 class Home extends React.PureComponent {
-  constructor(props) {
-    super(props)
-  }
-
-  addFile() {
-    const content = 'Some text to save into the file.'
-    dialog.showSaveDialog((filename) => {
-      if (filename === undefined) {
-        console.log('User did not save the file.')
-        return
-      }
-
-      fs.writeFile(filename, content, (err) => {
-        if (err) {
-          alert(`An error ocurred creating the file: ${err.message}`)
-        }
-
-        alert('The file has been successfully saved.')
-      })
-    })
-  }
-
   render() {
     return (
       <div>
         <h1>Files</h1>
         <button
           className="btn btn-large btn-primary"
-          onClick={this.addFile}
+          onClick={addFiles}
         >
           Add File
         </button>
