c60c99a4ceac56f10d4b8f1a8b098b61e25f0df4
Blake Hair <blake.hair@gmail.com>
2019-12-01 16:47:58 -0500

    :lipstick: Add a better focus state to tag items.

--- src/modules/media/components/tag-list.jsx
+++ src/modules/media/components/tag-list.jsx
@@ -1,51 +1,62 @@
-import isEmpty from 'lodash/isEmpty'
-import noop from 'lodash/noop'
-import PropTypes from 'prop-types'
-import React from 'react'
+import isEmpty from 'lodash/isEmpty';
+import noop from 'lodash/noop';
+import PropTypes from 'prop-types';
+import React from 'react';
 
 const Tag = ({ onTagClick, tag }) => (
   <a
-    className="nav-group-item"
+    className="nav-group-item tag"
     onClick={() => onTagClick(tag.id)}
+    onKeyPress={() => onTagClick(tag.id)}
     role="button"
     tabIndex={0}
   >
     {tag.name} {tag.count}
   </a>
-)
+);
 Tag.propTypes = {
   onTagClick: PropTypes.func.isRequired,
   tag: PropTypes.shape({
     count: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired
   }).isRequired
-}
+};
 
 const SearchedTags = props => (
   <div>
     <h5 className="nav-group-title">Searched Tags</h5>
-    {props.tags.map(tag => <Tag key={`search_${tag.name}`} onTagClick={props.onTagClick} tag={tag} />)}
+    {props.tags.map(tag => (
+      <Tag key={`search_${tag.name}`} onTagClick={props.onTagClick} tag={tag} />
+    ))}
   </div>
-)
+);
 SearchedTags.propTypes = {
   onTagClick: PropTypes.func.isRequired,
-  tags: PropTypes.arrayOf(PropTypes.shape({
-    count: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired
-  })).isRequired
-}
+  tags: PropTypes.arrayOf(
+    PropTypes.shape({
+      count: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired
+    })
+  ).isRequired
+};
 
 const TagList = props => (
   <div className="pane-sm sidebar">
     {props.children}
     <nav className="nav-group">
-      {!isEmpty(props.searchedTags) &&
-        <SearchedTags onTagClick={props.onSearchedTagClick} tags={props.searchedTags} />}
+      {!isEmpty(props.searchedTags) && (
+        <SearchedTags
+          onTagClick={props.onSearchedTagClick}
+          tags={props.searchedTags}
+        />
+      )}
       <h5 className="nav-group-title">All Tags</h5>
-      {props.tags.map(tag => <Tag key={`list_${tag.name}`} onTagClick={props.onTagClick} tag={tag} />)}
+      {props.tags.map(tag => (
+        <Tag key={`list_${tag.name}`} onTagClick={props.onTagClick} tag={tag} />
+      ))}
     </nav>
   </div>
-)
+);
 
 TagList.propTypes = {
   children: PropTypes.oneOfType([
@@ -54,21 +65,25 @@ TagList.propTypes = {
   ]),
   onSearchedTagClick: PropTypes.func,
   onTagClick: PropTypes.func.isRequired,
-  searchedTags: PropTypes.arrayOf(PropTypes.shape({
-    count: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired
-  })),
-  tags: PropTypes.arrayOf(PropTypes.shape({
-    count: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired
-  }))
-}
+  searchedTags: PropTypes.arrayOf(
+    PropTypes.shape({
+      count: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired
+    })
+  ),
+  tags: PropTypes.arrayOf(
+    PropTypes.shape({
+      count: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired
+    })
+  )
+};
 
 TagList.defaultProps = {
   children: null,
   onSearchedTagClick: noop,
   searchedTags: [],
   tags: []
-}
+};
 
-export default TagList
+export default TagList;
